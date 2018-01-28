e522575bc215b547cca29379944cb1681ded2146
wjsaunders <wjsaunders7@gmail.com>
2018-01-28 15:49:31 -0500

    Update c_pin.ino

--- sketch_microdrum/c_pin.ino
+++ sketch_microdrum/c_pin.ino
@@ -52,6 +52,51 @@ enum state:byte
 };
 //============================
 
+//This section is for digital pins used as chokes. This can be used with or without multiplexers, as long as the pins are not used for anything else.
+//An example for the choke note/name/pin here, plus the code for the bounce library when using a Teensy. Add as many as wanted following these examples.
+
+#if USE_DIG
+ #if TEENSY
+   // Define the note for the choke
+  #define Choke1 78
+
+   // Define the note for digital auxiliary choke
+  #define Aux1 47
+
+   // Define the pin for the choke
+ #define Choke1_Pin 4 
+
+   // Define the pin for the auxiliary choke
+ #define Aux1_Pin 8
+ 
+ #include <Bounce.h>
+  
+  Bounce Choke_A = Bounce(Choke1_Pin, 5); // 5 = 5 ms debounce time which is appropriate for good quality mechanical pushbuttons
+  Bounce AUX_A = Bounce(Aux1_Pin, 5);  // if a button is too "sensitive" to rapid touch, you can increase this time.
+
+ #else
+
+ // Define the note for the choke
+ #define choke1 78
+ 
+ // Define the note for digital auxiliary choke
+ #define Aux1 47
+
+ // Define the pin for the choke
+ #define Choke1_Pin 4 
+
+ // Define the pin for the auxiliary choke
+ #define Aux1_Pin 8
+
+//Define the initial state of the choke pins
+ int Choke1_State = LOW;
+
+ int Aux1_State = LOW;
+
+ int currentSwitchState = LOW;
+#endif
+#endif
+
 //===========================
 //   DEFAULT PIN
 //===========================
@@ -84,32 +129,32 @@ const byte DP_RIDEEDGE  = 0x0F;
 
 //*** Pinos Padr�es do Microdrum
 
-const byte DP_SNAREHEAD = 0x04;   // - tip
+const byte DP_SNAREHEAD = 0x04;
 const byte DP_SNARERIM  = 0x06;
-const byte DP_KICK      = 0x07;   // - tip
+const byte DP_KICK      = 0x07;
 const byte DP_EFFECT    = 0x05;
 const byte DP_HHBOW     = 0x03;
-const byte DP_HHEDGE    = 0x00;   // - tip
+const byte DP_HHEDGE    = 0x00;
 const byte DP_HHC       = 0x01;
-const byte DP_HHC_RING  = 0x02;   //might delete this. Don't know what it is really.
+const byte DP_HHC_RING  = 0x02;
 
-const byte DP_RIDEBOW   = 0x0C;   // pin4         pin2
-const byte DP_RIDEEDGE  = 0x0E;   // pin6 - tip   pin4
-const byte DP_TOM1HEAD  = 0x0F;   // pin7 - tip
-const byte DP_TOM1RIM   = 0x0D;   // pin5
-const byte DP_TOM2HEAD  = 0x0B;   // pin3 - tip
-const byte DP_TOM2RIM   = 0x08;   // pin0         pin1
-const byte DP_CRASHBOW  = 0x09;   // pin1         pin0
-const byte DP_CRASHEDGE = 0x0A;   // pin2 - tip   pin6
+const byte DP_RIDEBOW   = 0x0C;
+const byte DP_RIDEEDGE  = 0x0E;
+const byte DP_TOM1HEAD  = 0x0F;
+const byte DP_TOM1RIM   = 0x0D;
+const byte DP_TOM2HEAD  = 0x0B;
+const byte DP_TOM2RIM   = 0x08;
+const byte DP_CRASHBOW  = 0x09;
+const byte DP_CRASHEDGE = 0x0A;
 
-const byte DP_TOM3HEAD  = 0x14;   // pin4 - tip
-const byte DP_TOM3RIM   = 0x16;   // pin6
-const byte DP_TOM4HEAD  = 0x17;   // pin7 - tip
-const byte DP_TOM4RIM   = 0x15;   // pin5
-const byte DP_CRASH2BOW = 0x13;   // pin3
-const byte DP_CRASH2EDGE= 0x10;   // pin0 - tip
-const byte DP_CHINA     = 0x11;   // pin1 - tip
-const byte DP_SPLASH    = 0x12;   // pin2 - tip
+const byte DP_TOM3HEAD  = 0x14;
+const byte DP_TOM3RIM   = 0x16;
+const byte DP_TOM4HEAD  = 0x17;
+const byte DP_TOM4RIM   = 0x15;
+const byte DP_CRASH2BOW = 0x13;
+const byte DP_CRASH2EDGE= 0x10;
+const byte DP_CHINA     = 0x11;
+const byte DP_SPLASH    = 0x12;
 #endif
 
 //===========================
@@ -600,7 +645,9 @@ class pin
         
         #if SAM2695
         //if(Channel==10){
+        #if DEBUG
         Serial.println("sent to SAM!");
+        #endif
         synth.noteOn(fluxCHANNEL, Note, v);      // play a note
         synth.noteOff(fluxCHANNEL, Note);         // send note off
         /*}
